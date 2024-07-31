edf98a63e2e497ba83b21f84ed187d35d87b878a
Eduardo-Dev <duio.el@gmail.com>
2024-07-30 21:13:08 -0300

    - Criado a classe para exercicios de Matrizes. - Feito um exercicio de laço while para calcular a media de duas notas.

--- MasteringCSharp.Exercicios/Fundamentos/Modulo 1/Lacos.cs
+++ MasteringCSharp.Exercicios/Fundamentos/Modulo 1/Lacos.cs
@@ -1,5 +1,5 @@
 ﻿namespace MasteringCSharp.Exercicios
-{   public class Lacos
+{   internal class Lacos
     {
         /* Estudos de laços de repetição */
 
@@ -36,6 +36,34 @@
 
             mediaTurma = mediaNotas / contagemAluno;
             Console.WriteLine($"A media da turma é {mediaTurma}");
+        }
+
+        public static void CalculaMediaNotas()
+        {
+            double nota1, nota2, media;
+            nota1 = 0;
+            nota2 = 0;
+
+                Console.WriteLine("Digite nota 1");
+                nota1 += Convert.ToDouble(Console.ReadLine());
+                while (nota1 < 0 || nota1 > 10)
+                {
+                    Console.WriteLine("Erro! nota invalida.");
+                    nota1 += Convert.ToDouble(Console.ReadLine());
+                };
+
+
+                Console.WriteLine("Digite nota 2");
+                nota2 += Convert.ToDouble(Console.ReadLine());
+                while (nota2 < 0 || nota2 > 10)
+                {
+                    Console.WriteLine("Erro! nota invalida.");
+                    nota2 += Convert.ToDouble(Console.ReadLine());
+                };
+
+                media = (nota1 + nota2) / 2;
+
+                Console.WriteLine($"Media das notas é {media}");
         }
 
         #endregion
---
+++ MasteringCSharp.Exercicios/Fundamentos/Modulo 1/Matrizes.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasteringCSharp.Exercicios.Fundamentos.Modulo_1
+{
+    internal class Matrizes
+    {        
+        public static void ManipulandoMatrizes()
+        {
+            int[,] matriz = { { 1, 2, }, { 3, 4 }, { 5, 6 } };
+
+            for (int linha = 0; linha < 3; linha++)
+            {
+                for (int coluna = 0; coluna < 2; coluna++)
+                {
+                    Console.WriteLine($"{linha},{coluna} => {matriz[linha, coluna]}\t");
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+        public static void SomarMatrizes()
+        {
+            int[,] matriz = { { 1, 2, }, { 3, 4 }, { 5, 6 } };
+            int soma = 0;
+
+            for (int linha = 0; linha < 3; linha++)
+            {
+                for (int coluna = 0; coluna < 2; coluna++)
+                {
+                    soma = soma + matriz[linha, coluna];
+                    Console.WriteLine($"{linha},{coluna} => {matriz[linha, coluna]}\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(soma);
+
+
+        }
+
+        public static void TrocarMatrizesParesPorZero()
+        {
+            int[,] matriz = { { 45, 12, }, { 22, 4 }, { 5, 6 } };
+            int divisaoMatrizes = 0;
+
+            for (int linha = 0; linha < 3; linha++)
+            {
+                for (int coluna = 0; coluna < 2; coluna++)
+                {
+                    divisaoMatrizes = matriz[linha,coluna] % 2;
+                    if (divisaoMatrizes == 1)
+                    {
+                        matriz[linha,coluna] = 1;
+                    }
+                    else
+                    {
+                        matriz[linha, coluna] = 0;
+                    }
+                    Console.WriteLine($"{linha},{coluna} => {matriz[linha, coluna]}\t");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
